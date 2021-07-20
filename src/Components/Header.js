10d2dc66a924a5484ba368026cc4afd7f5d99258
gaddamsunil44 <gaddamsunil3@gmail.com>
2021-07-20 12:02:28 +0530

    me

---
+++ src/Components/Header.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
+import Fade from "react-reveal";
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef()  
+    this.scrollData = this.scrollData.bind(this)
+  }
+
+
+  componentDidMount() {
+    this.scrollData()
+  }
+
+  scrollData() {
+    console.log(this)
+    window.scrollTo(0,  this?.myRef?.current?.offsetTop)
+  }
+
+  render() {
+    if (!this.props.data) return null;
+
+    const project = this.props.data.project;
+    const github = this.props.data.github;
+    const name = this.props.data.name;
+    const description = this.props.data.description;
+
+// "color"
+// "ball"
+// "lines"
+// "thick"
+// "circle"
+// "cobweb"
+// "polygon"
+// "square"
+// "tadpole"
+// "fountain"
+// "random"
+// "custom"
+
+    return (
+      <header id="home">
+        <ParticlesBg type="lines" bg={true} />
+
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+         
+            <li className="current">
+              <a id='home1' className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a onClick={this.scrollData} id='portfolio1' className="smoothscroll" href="#articles">
+                Works
+              </a>
+            </li>
+            <li>
+              <a id='about1' className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+
+            {/* <li>
+              <a id='resume1' className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li> */}
+          
+
+            {/* <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li> */}
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <Fade bottom>
+              <h1 className="responsive-headline">{name}</h1>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}</h3>
+            </Fade>
+            <hr />
+            <Fade bottom duration={2000}>
+              {/* <ul className="social">
+                <a href={project} className="button btn project-btn">
+                  <i className="fa fa-book"></i>Project
+                </a>
+                <a href={github} className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul> */}
+            </Fade>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
